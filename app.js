94a9f790cc21f3af592753b07b9c2ed3c7bbbcfa
Frank GP <fgp555@gmail.com>
2024-03-04 09:47:10 -0500

    create api

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express")
+const tempData = require("./data/tempData.js")
+
+// console.log(tempData);
 
 const app = express()
 
 app.use(express.static("frontend"))
+
+app.get("/movies", (req, res) => {
+    res.json(tempData)
+})
 
 app.listen(3000)
---
+++ data/tempData.js
@@ -0,0 +1,23 @@
+const tempData = [
+    {
+      title: "Guardians of the Galaxy Vol. 2",
+      year: 2017,
+      director: "James Gunn",
+      duration: "2h 16min",
+      genre: ["Action", "Adventure", "Comedy"],
+      rate: 7.7,
+      poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+    },
+    {
+      title: "Star Wars: Episode IV - A New Hope",
+      year: 1977,
+      director: "George Lucas",
+      duration: "2h 1min",
+      genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+      rate: 8.7,
+      poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+    },
+  ];
+  
+  module.exports = tempData;
+  
